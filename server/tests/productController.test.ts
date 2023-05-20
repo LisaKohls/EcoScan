79c643d639da18732e8f2fcd0ee1e51a6bf0bdc3
Lea Bretz <leabretz7@gmail.com>
2023-05-20 17:02:00 +0200

    WIP Progress Add test for init products route

--- server/tests/productController.test.ts
+++ server/tests/productController.test.ts
@@ -1,4 +1,6 @@
 import {getInitialProducts} from "../src/controllers/productController";
+import request = require('supertest');
+import app from '../src/app';
 
 describe('getInitialProducts', () => {
     it('Should return a list of products', () => {
@@ -6,3 +8,11 @@ describe('getInitialProducts', () => {
         expect(products.length).toBe(3)
     })
 })
+
+describe('POST /api/product/init', () => {
+    it('should return a JSON message', async () => {
+        const response = await request(app).post('/api/product/init');
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual('Initial Products successfully created');
+    });
+});
