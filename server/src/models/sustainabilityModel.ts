e3e0e81fb1243f67f51f095aa258dc867c2139c2
Lea Bretz <leabretz7@gmail.com>
2023-05-20 09:46:56 +0200

    Fix sustainabilityModel and add model that represents the init format from the json file

--- server/src/models/sustainabilityModel.ts
+++ server/src/models/sustainabilityModel.ts
@@ -1,7 +1,30 @@
-import { Document, Schema } from 'mongoose'
+import mongoose, { Document, Schema } from 'mongoose'
+import { IProductInitialFormat } from './productModel'
 
 export interface ISustainability extends Document {
   name: string;
+  eco_chemicals: number;
+  eco_lifetime: number;
+  eco_water: number;
+  eco_inputs: number;
+  eco_quality: number;
+  eco_energy: number;
+  eco_waste_air: number;
+  eco_environmental_management: number;
+  social_labour_rights: number;
+  social_business_practice: number;
+  social_social_rights: number;
+  social_company_responsibility: number;
+  social_conflict_minerals: number;
+  ref: string;
+}
+
+export interface ISustainabilityInitialFormat {
+  id: string;
+  timestamp: string;
+  name: string;
+  description: string;
+  cred_credibility: number;
   eco_chemicals: number;
   eco_lifetime: number;
   eco_water: number;
@@ -28,50 +51,59 @@ export const SustainabilitySchema: Schema = new Schema({
   },
   eco_lifetime: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_water: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_inputs: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_quality: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_energy: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_waste_air: {
     type: Number,
-    required: true
+    default: 0
   },
   eco_environmental_management: {
     type: Number,
-    required: true
+    default: 0
   },
   social_labour_rights: {
     type: Number,
-    required: true
+    default: 0
   },
   social_business_practice: {
     type: Number,
-    required: true
+    default: 0
   },
   social_social_rights: {
     type: Number,
-    required: true
+    default: 0
   },
   social_company_responsibility: {
     type: Number,
-    required: true
+    default: 0
   },
   social_conflict_minerals: {
     type: Number,
+    default: 0
+  },
+  ref: {
+    type: String,
     required: true
   }
 })
+
+export const Sustainability = mongoose.model<ISustainability>(
+  'Sustainability',
+  SustainabilitySchema
+)
