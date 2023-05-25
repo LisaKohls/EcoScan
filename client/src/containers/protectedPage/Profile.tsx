60e55a11a6b7262114ba014a43c4caa969c86a93
jb266 <jb266@hdm-stuttgart.de>
2023-05-25 17:04:23 +0200

    finished profile prototype styling

--- client/src/containers/protectedPage/Profile.tsx
+++ client/src/containers/protectedPage/Profile.tsx
@@ -1,64 +1,44 @@
 import React from 'react';
 import { ProfileProps } from './ProfileProps';
-import { HiLibrary, HiHome } from 'react-icons/hi';
-import { IoIosQrScanner } from 'react-icons/Io';
 import { useNavigate } from 'react-router-dom';
-
+import BottomNavBar from '../../components/BottomNavBar';
 
 const Profile: React.FC<ProfileProps> = props => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navigateToProductInfo = () => {
-        navigate('/productInfo');
-        };
+  const logout = () => {
+    navigate('/login');
+  };
 
-    const naviagteToProfile = () => {
-        navigate('/profile');
-        };
-
-    
-    
-    
-    return (
-        <div className="min-h-screen bg-orange-50-50">
-          <div className="bg-lime-100 bg- text-black rounded-b-3xl">
-            <h1 className="text-left text-xl font-normal p-8 pl-24 ">
-                {props.name}
-            </h1>
-            <h2 className="text-left text-xl font-normal p-8 pl-24 ">
-                {props.email}
-            </h2>
+  return (
+    <div className="min-h-screen bg-orange-50-50">
+      <div className="bg-lime-100 bg-text-black rounded-b-3xl flex pt-8 pb-8">
+        <img
+          src={props.img}
+          alt={props.name}
+          className="w-32 h-32 border ms-4 me-4 border-gray-400 rounded-full"
+        />
+        <div className="flex flex-col justify-center">
+          <div className="ml-8">
+            <h1 className="text-left text-xl font-normal">{props.name}</h1>
+            <h2 className="text-left text-xl font-normal pt-2">{props.email}</h2>
+          </div>
+          <div className="flex justify-center">
             <button
-            className="text-white bg-emerald-800 rounded-md text-center  mt-8 p-2"
-            onClick={() => navigate(-1)}
+              className="text-white bg-emerald-800 rounded-md text-center mt-8 mb-8 p-2 pl-6 pr-6"
+              onClick={() => logout()}
             >
-                Logout
+              Logout
             </button>
           </div>
-          <nav className="fixed bottom-4 left-1 right-1 m-1 p-4 bg-lime-100 flex items-center justify-between rounded-lg">
-            <div className="flex flex-col items-center text-balck">
-            <HiLibrary className="text-2xl" />
-            <span>Favorites</span>
-            </div>
-            <div
-            className="flex flex-col items-center text-white"
-            role="button"
-            tabIndex={0}
-            id="arrowIcon"
-            onClick={() => navigateToProductInfo()}
-            onKeyDown={event => {
-                console.log(event.key);
-            }}
-            >
-            <IoIosQrScanner className="absolute bottom-6 rounded-full w-16 h-16  bg-emerald-800 p-4" />
-            </div>
-            <div className="flex flex-col items-center text-black" onClick={() => naviagteToProfile()}>
-            <HiHome className="text-2xl" />
-            <span>Profile</span>
-            </div>
-      </nav>
         </div>
-      );
+      </div>
+
+      <h1 className="text-left text-xl font-normal mt-8 ml-6">Added Products</h1>
+
+      <BottomNavBar />
+    </div>
+  );
 };
 
 export default Profile;
