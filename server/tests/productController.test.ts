24f4e8067534ffd146602368e344470611a596cc
Lea Bretz <leabretz7@gmail.com>
2023-05-29 10:54:53 +0200

    Optimize imports in productController tests

--- server/tests/productController.test.ts
+++ server/tests/productController.test.ts
@@ -1,6 +1,4 @@
 import {getInitialProducts} from "../src/controllers/productController";
-import request = require('supertest');
-import app from '../src/app';
 
 describe('getInitialProducts', () => {
     it('Should return a list of products', () => {
