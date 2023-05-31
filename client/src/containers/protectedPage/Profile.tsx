a413ed34b79c6f73f1d075476beaacd0e1ac82a8
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-05-31 09:51:08 +0200

    search icon

--- client/src/containers/protectedPage/Profile.tsx
+++ client/src/containers/protectedPage/Profile.tsx
@@ -12,29 +12,33 @@ const Profile: React.FC<ProfileProps> = props => {
 
   return (
     <div className="min-h-screen bg-orange-50-50">
-        <div className="bg-lime-100 bg-text-black rounded-b-3xl flex pt-8 pb-8">
-            <img
-                src={props.img}
-                alt={props.name}
-                className="w-32 h-32 border ms-4 me-4 border-gray-400 rounded-full"
-            />
-            <div className="flex flex-col justify-center">
-                <div className="ml-8">
-                    <h1 className="text-left text-xl font-normal">{props.name}</h1>
-                    <h2 className="text-left text-xl font-normal pt-2">{props.email}</h2>
-                </div>
-                <div className="flex justify-center">
-                    <button
-                    className="text-white bg-emerald-800 rounded-md text-center mt-8 mb-8 p-2 pl-6 pr-6"
-                    onClick={() => logout()}
-                    >
-                    Logout
-                    </button>
-                </div>
-            </div>
+      <div className="bg-lime-100 bg-text-black rounded-b-3xl flex pt-8 pb-8">
+        <img
+          src={props.img}
+          alt={props.name}
+          className="w-32 h-32 border ms-4 me-4 border-gray-400 rounded-full"
+        />
+        <div className="flex flex-col justify-center">
+          <div className="ml-8">
+            <h1 className="text-left text-xl font-normal">{props.name}</h1>
+            <h2 className="text-left text-xl font-normal pt-2">
+              {props.email}
+            </h2>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="text-white bg-emerald-800 rounded-md text-center mt-8 mb-8 p-2 pl-6 pr-6"
+              onClick={() => logout()}
+            >
+              Logout
+            </button>
+          </div>
         </div>
+      </div>
 
-      <h1 className="text-left text-xl font-normal mt-8 ml-6">Added Products</h1>
+      <h1 className="text-left text-xl font-normal mt-8 ml-6">
+        Added Products
+      </h1>
 
       <BottomNavBar />
     </div>
--- client/src/containers/protectedPage/SearchForProduct.tsx
+++ client/src/containers/protectedPage/SearchForProduct.tsx
@@ -1,20 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useState } from 'react';
+import Header from '../../components/Header';
+import {FiArrowLeft} from "react-icons/fi";
+import {useNavigate} from "react-router-dom";
+import SearchBar from "../../components/SearchBar";
 
 const SearchForProduct = () => {
-  const [barcodeNumber, setBarcodeNumber] = useState('');
+
+  const navigate = useNavigate();
   return (
     <div>
-      <p>Search For Product</p>
-
+      <Header title="Search For Product" />
+        <button
+            className="fixed top-0 left-0 ps-4 pt-8"
+            onClick={() => navigate(-1)}
+        >
+            <FiArrowLeft className="inline-block text-white text-2xl" />
+        </button>
       <div className="p-4">
-        <input
-          className="w-full p-2 bg-white text-black border-2 border-green-400 rounded-md"
-          type="text"
-          placeholder="Enter barcode number"
-          value={barcodeNumber}
-          onChange={e => setBarcodeNumber(e.target.value)}
-        />
+          <SearchBar/>
       </div>
     </div>
   );
