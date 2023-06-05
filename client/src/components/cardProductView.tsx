dccfde0cb7aae33a2db695def85cda00dbfb9163
janaeichholz <je067@hdm-stutgart.de>
2023-06-05 09:27:32 +0200

    added style to card product view

--- client/src/components/cardProductView.tsx
+++ client/src/components/cardProductView.tsx
@@ -5,12 +5,12 @@ import { useNavigate } from 'react-router-dom';
 const CardProductView: React.FC<ProductInfoProps> = props => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-between mx-10 mt-10">
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 hover:scale-125">
       <button onClick={() => navigate(-1)}>
         <img src={props.img} alt={props.name} />
-        <div className="">
-          <h5 className="">{props.name}</h5>
-          <p className="">{props.index}</p>
+        <div>
+          <h5 className="text-xl font-medium text-black">{props.name}</h5>
+          <p className="font-medium text-black">{props.index}</p>
         </div>
       </button>
     </div>
