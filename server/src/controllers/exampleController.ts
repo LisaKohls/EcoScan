4d81c832051fb35c92e7c98e50b415cc2b3bc718
Lea Bretz <leabretz7@gmail.com>
2023-06-06 00:01:21 +0200

    Remove unused example controller

--- server/src/controllers/exampleController.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export const getExample = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.json({ message: 'Hello from the example controller!' })
-  } catch (err) {
-    next(err)
-  }
-}
