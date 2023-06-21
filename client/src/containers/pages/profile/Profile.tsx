8a3de6f7c5de0ac58efdd3474a3209d89af35864
Singer Steffen <ss546@hdm-stuttgart.de>
2023-06-21 23:42:23 +0200

    Resolve "profile show real username and no added products"

--- client/src/containers/pages/profile/Profile.tsx
+++ client/src/containers/pages/profile/Profile.tsx
@@ -1,14 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 import useLogout from '../../../hooks/useLogout';
+import useAuth from '../../../hooks/useAuth';
+import { ReactElement, FC } from 'react';
 import ButtonPrimary from '../../../components/buttons/ButtonPrimary';
 
-const Profile = () => {
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+}
+
+const Profile: FC = (): ReactElement => {
   const exampleData = {
-    name: 'Max Mustermann',
-    email: 'max.mustermal@examle.com',
+    email: 'max.musterman@examle.com',
     img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
   };
 
+  const auth = useAuth();
   const navigate = useNavigate();
   const logout = useLogout();
 
@@ -17,32 +25,51 @@ const Profile = () => {
     navigate('/login');
   };
 
+  // Mock data - replace it with real data
+  const products: Product[] = [];
+
+  const addProduct = () => {
+    // Implementation of adding a new product
+    console.log('Add product');
+  };
+
   return (
     <>
-      <div className="bg-lime-100 bg-secondary-color rounded-b-3xl flex pt-8 pb-8">
+      <div className="flex flex-col items-center justify-center bg-lime-100 p-4 rounded-bl-3xl">
         <img
           src={exampleData.img}
-          alt={exampleData.name}
-          className="w-32 h-32 border ms-4 me-4 border-gray-400 rounded-full"
+          alt="profile_photo"
+          className="w-24 h-24 border-2 border-gray-400 rounded-full mt-4"
         />
-        <div className="flex flex-col justify-center">
-          <div className="ml-8">
-            <h1 className="text-left text-xl font-normal">
-              {exampleData.name}
-            </h1>
-            <h2 className="text-left text-xl font-normal pt-2">
-              {exampleData.email}
-            </h2>
-          </div>
-          <div className="flex justify-center">
-            <ButtonPrimary onClick={() => signOut()}>Logout</ButtonPrimary>
-          </div>
+        <h1 className="text-center text-xl font-medium mt-4">
+          {auth.auth.username}
+        </h1>
+        <h2 className="text-center text-base font-light mt-2">
+          {exampleData.email}
+        </h2>
+        <div className="w-full flex justify-center mt-4">
+          <button onClick={() => signOut()} className="w-full sm:w-auto">
+            Logout
+          </button>
         </div>
       </div>
-
-      <h1 className="text-left text-xl font-normal mt-8 ml-6">
-        Added Products
-      </h1>
+      <h1 className="text-center text-xl font-medium mt-8">Added Products</h1>
+      {products.length > 0 ? (
+        products.map((product: Product) => (
+          <div
+            key={product.id}
+            className="flex justify-between items-center p-4 bg-gray-100 m-2 rounded"
+          >
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))
+      ) : (
+        <div className="flex flex-col items-center justify-center mt-4">
+          <p className="mb-4">You haven&apos;t added any products yet.</p>
+          <ButtonPrimary onClick={addProduct}>Add Product</ButtonPrimary>
+        </div>
+      )}
     </>
   );
 };
