214f9817a64dc14f3ead9d287370ee568477b72e
Steffen Singer <info.stapps@gmail.com>
2023-06-22 10:24:25 +0200

    fixed link product card

--- client/src/components/productcard/ProductCard.tsx
+++ client/src/components/productcard/ProductCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HeartFavorites from '../buttons/ButtonHeart';
+import { Link } from 'react-router-dom';
 
 interface ProductCardProps {
   name: string;
@@ -17,19 +18,25 @@ const ProductCard: React.FC<ProductCardProps> = ({
   favorite,
 }) => {
   return (
-    <div className="bg-white shadow-md rounded-md overflow-hidden max-w-xs mx-auto">
-      <img className="w-full h-64 object-cover" src={image} alt={name} />
-      <div className="p-4">
-        <h2 className="text-lg font-semibold mb-2 whitespace-normal">{name}</h2>
-        <p className="text-gray-600 text-sm whitespace-normal">{description}</p>
-      </div>
-      <div className="flex justify-between items-center p-4 border-t">
-        <div className="text-sm">
-          <span className="font-semibold">Barcode:</span> {barcode}
+    <Link to={`/product/${barcode}`} className="cursor-pointer">
+      <div className="cursor-pointer bg-white shadow-md rounded-md overflow-hidden max-w-xs mx-auto">
+        <img className="w-full h-64 object-cover" src={image} alt={name} />
+        <div className="p-4">
+          <h2 className="text-lg font-semibold mb-2 whitespace-normal">
+            {name}
+          </h2>
+          <p className="text-gray-600 text-sm whitespace-normal">
+            {description}
+          </p>
         </div>
-        <HeartFavorites barcode={barcode} isInitiallyFavorite={favorite} />
+        <div className="flex justify-between items-center p-4 border-t">
+          <div className="text-sm">
+            <span className="font-semibold">Barcode:</span> {barcode}
+          </div>
+          <HeartFavorites barcode={barcode} isInitiallyFavorite={favorite} />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
