b9b344bb18d1b75fcb8c9120fd44caf17a83eaa7
lisakohls <lk210@hdm-stuttgart.de>
2023-07-03 11:40:27 +0200

    showing loading on load

--- client/src/components/search/SearchResultList.tsx
+++ client/src/components/search/SearchResultList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import AddProduct from '../../containers/popupcontent/addproduct/AddProduct';
 import PopUp from '../../containers/layouts/popup/PopUp';
 import ButtonPrimary from '../buttons/ButtonPrimary';
@@ -8,15 +8,19 @@ import ProductContainer from '../../containers/productcontainer/ProductContainer
 const SearchResultList = ({
   products,
   searchQuery,
+  loading,
 }: {
   products: Product[];
   searchQuery: string;
+  loading: boolean;
 }) => {
   const [addProductPopUp, setAddProductPopUp] = useState(false);
 
   return (
     <div className="pt-4 m-margin-elements">
-      {products.length !== 0 ? (
+      {loading ? (
+        <p className="mt-2 text-center text-gray-500 ">Loading...</p>
+      ) : products.length !== 0 ? (
         <ProductContainer products={products} />
       ) : (
         <div className="mt-4 text-center">
