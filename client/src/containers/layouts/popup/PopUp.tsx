782d2a94cbcc43f1dcf6ffe9b6bf5e5e73331b7c
janaeichholz <je067@hdm-stutgart.de>
2023-07-03 16:23:14 +0200

    added view of add product

--- client/src/containers/layouts/popup/PopUp.tsx
+++ client/src/containers/layouts/popup/PopUp.tsx
@@ -5,7 +5,7 @@ const PopUp: React.FC<PopUpProps> = props => {
   const { children } = props;
   return props.trigger ? (
     <div className="fixed z-10 top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center overflow-hidden">
-      <div className="absolute top-20 bottom-0 right-1/2 left-1/2 transform -translate-x-1/2 p-0 w-auto h-auto bg-textColor overflow-y-hidden">
+      <div className="items-center bg-primary-color  rounded-lg">
         <button
           className="absolute top-0 right-0 p-1/2 cursor-pointer bg-transparent border-none outline-none text-mainColor text-1.8xl"
           onClick={() => {
--- client/src/containers/popupcontent/addproduct/AddProduct.tsx
+++ client/src/containers/popupcontent/addproduct/AddProduct.tsx
@@ -1,43 +1,133 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import axios from 'axios';
 
 interface AddProductProps {
   setTrigger: Dispatch<SetStateAction<boolean>>;
 }
 
 const AddProduct: React.FC<AddProductProps> = props => {
-  const add = () => {
-    //component is closing
-    props.setTrigger(false);
+  const [barcode, setBarcode] = useState('');
+  const [name, setName] = useState('');
+  const [sustainabilityEco, setsustainabilityEco] = useState('');
+  const [sustainabilitySocial, setsustainabilitySocial] = useState('');
+  const [description, setDescription] = useState('');
+
+  const add = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault(); // Verhindert das Absenden des Formulars
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6eyJ1c2VybmFtZSI6ImphbmEiLCJyb2xlcyI6WzUxNTAsMjAwMV19LCJpYXQiOjE2ODgwMzY4MTQsImV4cCI6MTY4ODAzODYxNH0.iv7h-2Q9ntEgTdw0EhY9ADnDxjbnTPVrU2_FTwmCKYs'}`,
+      },
+    };
+
+    try {
+      await axios.post(
+        '/api/product/add',
+        {
+          barcode: '378',
+          categories: [],
+          description: 'Bla Bla Bla',
+          imageUrls: [],
+          sustainabilityName: 'sustainabilityName',
+          name: 'Product 1',
+          sustainabilitySocial: 20,
+          sustainabilityEco: 20,
+        },
+        config
+      );
+      // Produkt erfolgreich hinzugefügt
+      props.setTrigger(false);
+    } catch (error) {
+      // Fehler beim Hinzufügen des Produkts
+      console.error(error);
+    }
   };
+
   console.log('pop up add new product opened');
-  //das ganze input Zeug ist noch nicht sichtbar aber component wird schon geöffnet
+  // das ganze input Zeug ist noch nicht sichtbar, aber die Komponente wird schon geöffnet
   return (
-    <div className="text-mainColor z-10 flex flex-col items-center p-4 overflow-auto">
-      <div className="block mt-4 text-left">
-        <h2 className="mt-5 text-center">Create new product</h2>
-        <input
-          className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
-          type="text"
-          placeholder="Enter barcode number"
-        />
-        <input
-          className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
-          type="text"
-          placeholder="Enter name of product"
-        />
-        <input
-          className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
-          type="number"
-          placeholder="Enter sustainability index"
-        />
-        <div className="flex justify-center">
-          <button
-            className="p-3 m-2 bg-grey-green text-white rounded-md"
-            onClick={add}
-          >
-            Add
-          </button>
-        </div>
+    <div className="text-black z-10 flex flex-col p-4 overflow-auto">
+      <h2 className="mt-5 text-center text-white text-xl">
+        Create new product
+      </h2>
+      <div className="pt-4 text-white">
+        <label>
+          Barcode Number
+          <div className="pt-2">
+            <input
+              className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
+              type="text"
+              placeholder="Enter barcode number"
+              value={barcode}
+              onChange={e => setBarcode(e.target.value)}
+            />
+          </div>
+        </label>
+      </div>
+      <div className="pt-4 text-white">
+        <label>
+          Name of Product
+          <div className="pt-2">
+            <input
+              className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
+              type="text"
+              placeholder="Enter name of product"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+        </label>
+      </div>
+      <div className="pt-4 text-white">
+        <label>
+          Sustainability index
+          <div className="pt-2">
+            <input
+              className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
+              type="number"
+              placeholder="Enter sustainability index"
+              value={sustainabilitySocial}
+              onChange={e => setsustainabilitySocial(e.target.value)}
+            />
+          </div>
+        </label>
+      </div>
+      <div className="pt-4 text-white">
+        <label>
+          Sustainability eco
+          <div className="pt-2">
+            <input
+              className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
+              type="number"
+              placeholder="Enter sustainability index"
+              value={sustainabilityEco}
+              onChange={e => setsustainabilityEco(e.target.value)}
+            />
+          </div>
+        </label>
+      </div>
+      <div className="pt-4 text-white">
+        <label>
+          Description of Product
+          <div className="pt-2">
+            <input
+              className="w-full p-2 bg-white text-black border-2 border-grey rounded-md focus:outline-none focus:border-black"
+              type="text"
+              placeholder="Enter description of product"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </div>
+        </label>
+      </div>
+      <div className="flex justify-center pt-2">
+        <button
+          className="p-3 px-16 m-5 bg-secondary-color text-black rounded-md"
+          onClick={add}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
