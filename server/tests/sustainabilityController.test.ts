55a880d803167e7b408655cdbcbfce45c5b9069e
Steffen Singer <info.stapps@gmail.com>
2023-07-08 14:53:28 +0200

    added unit test and make prettier format it

--- server/tests/sustainabilityController.test.ts
+++
@@ -1,8 +0,0 @@
-import { getInitialSustainabilities } from '../src/controllers/sustainabilityController';
-
-describe('getInitialSustainabilities', () => {
-    it('Should return a list of sustainability', () => {
-        const sustainabilities = getInitialSustainabilities()
-        expect(sustainabilities.length).toBe(2)
-    })
-})
