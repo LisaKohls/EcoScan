128a1127fa520d8e82e72d63dd8cb42e02cd4a68
Steffen Singer <info.stapps@gmail.com>
2023-07-09 07:50:01 +0200

    responsive product detail view, more details displayed

--- client/src/components/search/SearchBar.tsx
+++ client/src/components/search/SearchBar.tsx
@@ -47,16 +47,20 @@ const SearchBar = ({
   }, [searchQuery]);
 
   return (
-    <div className="relative flex items-center rounded-md bg-white border border-gray-300 mx-margin-elements">
-      <FaSearch className="absolute left-3 text-gray-400" />
-      <input
-        ref={searchInput}
-        className="w-full py-2 pr-4 pl-10 text-black rounded-md focus:outline-none focus:border-transparent focus:ring-2 focus:ring-primary-color"
-        type="text"
-        placeholder="Enter barcode number or product name"
-        value={bufSearchQuery}
-        onChange={e => handleChange(e)}
-      />
+    <div className="flex justify-center">
+      <div className="max-w-lg w-full">
+        <div className="relative flex items-center rounded-md bg-white border border-gray-300 mx-margin-elements">
+          <FaSearch className="absolute left-3 text-gray-400" />
+          <input
+            ref={searchInput}
+            className="w-full py-2 pr-4 pl-10 text-black rounded-md focus:outline-none focus:border-transparent focus:ring-2 focus:ring-primary-color"
+            type="text"
+            placeholder="Enter barcode number or product name"
+            value={bufSearchQuery}
+            onChange={e => handleChange(e)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
